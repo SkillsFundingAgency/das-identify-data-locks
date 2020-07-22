f0e12a50f3f8ce9eb90c31a81be26cb6346b739b
Ben Arnold <ben@arnoldfamily.me.uk>
2020-07-22 05:04:59 +0100

    Model from Payments

---
+++ src/LearnerDataMismatches/Model/Payment.cs
@@ -0,0 +1,18 @@
+﻿namespace PaymentTools.Model
+{
+    public class Payment : ICommitmentItem
+    {
+        public long Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string TransactionType { get; set; }
+
+        public string Type => "Payment";
+
+        public byte DeliveryPeriod { get; set; }
+
+        public string PriceEpisodeIdentifier { get; internal set; }
+        public SFA.DAS.Payments.Model.Core.CollectionPeriod CollectionPeriod { get; internal set; }
+    }
+}
--- src/LearnerDataMismatches/Startup.cs
+++ src/LearnerDataMismatches/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SFA.DAS.Payments.Application.Repositories;
 
 namespace LearnerDataMismatches
 {
@@ -23,6 +25,11 @@ namespace LearnerDataMismatches
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PaymentsDataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PaymentsDatabase"))
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+            services.AddScoped<IPaymentsDataContext, PaymentsDataContext>();
+
             services.AddRazorPages();
         }
 
