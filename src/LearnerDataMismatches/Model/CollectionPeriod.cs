ffcc9c2adb241d019b853e39dc145a650f2d2c38
Ben Arnold <ben@arnoldfamily.me.uk>
2020-07-22 05:06:32 +0100

    Payments style and GDS style Learner Reports

--- src/LearnerDataMismatches/Model/CollectionPeriod.cs
+++ src/LearnerDataMismatches/Model/CollectionPeriod.cs
@@ -10,5 +10,16 @@ namespace PaymentTools.Model
         public decimal TotalLocked => PriceEpisodes.SelectMany(x => x.Commitments).SelectMany(x => x.Items).Select(x => x as DataLock).Where(x => x != null).Sum(x => x.Amount);
         public List<PriceEpisode> PriceEpisodes { get; set; }
 
+        public List<DataLock> DataLocks => PriceEpisodes
+            .SelectMany(x => x.Commitments)
+            .SelectMany(x => x.DataLocked)
+            .ToList();
+
+        public List<string> UniqueDataLockNames =>
+            DataLocks
+            .Select(l => l.DataLockErrorCode.ToString())
+            .Distinct()
+            .ToList();
+
     }
 }
