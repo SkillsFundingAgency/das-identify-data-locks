24e73bd91d00d79f7f4dc3c51c2903b4252abe1b
dev4valtech <dev4valtech@gmail.com>
2020-07-30 11:51:37 +0100

    AD-2201 Added validation to start page

--- src/SFA.DAS.LearnerDataMismatches.Web/Pages/Start.cshtml.cs
+++ src/SFA.DAS.LearnerDataMismatches.Web/Pages/Start.cshtml.cs
@@ -1,14 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SFA.DAS.LearnerDataMismatches.Web.Pages
 {
     public class StartModel : PageModel
     {
-        //[BindProperty]
-        public string Uln { get; set; }
+        [BindProperty]
+        public int? Uln { get; set; }
 
-        public void OnGet()
+        public IActionResult OnPost()
         {
+            ValidateModel();
+            if(ModelState.IsValid)
+            {
+                return RedirectToPage("learner");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        private void ValidateModel()
+        {
+            if(Uln == null || Uln <= 0 || Uln > 9999999999)
+            {
+                ModelState.AddModelError(nameof(Uln), "Enter a valid ULN");
+            }
         }
     }
 }
