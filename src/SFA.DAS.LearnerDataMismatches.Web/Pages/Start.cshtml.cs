8acef0cdcc3b2a7448ac42da095361a4aa584baf
dev4valtech <dev4valtech@gmail.com>
2020-08-25 10:31:08 +0100

    :sparkles: AD-2301  Additional validation to Uln

--- src/SFA.DAS.LearnerDataMismatches.Web/Pages/Start.cshtml.cs
+++ src/SFA.DAS.LearnerDataMismatches.Web/Pages/Start.cshtml.cs
@@ -1,17 +1,25 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SFA.DAS.LearnerDataMismatches.Web.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.Application.Repositories;
 
 namespace SFA.DAS.LearnerDataMismatches.Web.Pages
 {
     public class StartModel : PageModel
     {
+        private readonly IPaymentsDataContext _context;
         [BindProperty]
         public string Uln { get; set; }
 
-        public IActionResult OnPost()
+        public StartModel(IPaymentsDataContext context) =>
+            _context = context;
+
+        public async Task<IActionResult> OnPost()
         {
-            ValidateModel();
+            await ValidateModel();
             if(ModelState.IsValid)
             {
                 return RedirectToPage("learner", new { Uln = Uln });
@@ -22,13 +30,26 @@ namespace SFA.DAS.LearnerDataMismatches.Web.Pages
             }
         }
 
-        private void ValidateModel()
+        private async Task ValidateModel()
         {
             var isUlnANumber = long.TryParse(Uln, out long uln);
             if(Uln == null || isUlnANumber == false || uln <= 0 || uln > 9999999999)
             {
                 ModelState.AddModelError(nameof(Uln), "Enter a valid ULN");
             }
+            else
+            {
+                var apprenticeship = await _context
+                    .Apprenticeship
+                    .Where(x => x.Uln == uln)
+                    .ToListAsync();
+                
+                if (!apprenticeship.Any())
+                {
+                    ModelState.AddModelError(nameof(Uln), "Uln not found");
+                }
+            }
+
         }
     }
 }
