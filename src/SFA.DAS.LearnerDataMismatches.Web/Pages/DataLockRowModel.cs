31548cc30b6d7e40d5c56b5bc76b88f3d26069ac
Ben Arnold <ben@arnoldfamily.me.uk>
2020-08-27 16:26:28 +0100

    ✨Data lock mismatch row

---
+++ src/SFA.DAS.LearnerDataMismatches.Web/Pages/DataLockRowModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SFA.DAS.LearnerDataMismatches.Web.Pages
+{
+    public class DataLockRowModel
+    {
+        public string Heading { get; }
+
+        public string ActiveDataLock { get; }
+
+        public string ApprenticeValue =>
+            valueExtractor?.Invoke(period.Apprenticeship)?.ToString();
+
+        public string IlrValue =>
+            valueExtractor?.Invoke(period.Ilr)?.ToString();
+
+        public bool IsLocked { get; }
+
+        private readonly Domain.CollectionPeriod period;
+        private readonly Func<Domain.DataMatch, object> valueExtractor;
+
+        public DataLockRowModel(
+            string heading,
+            Domain.DataLock data,
+            Domain.CollectionPeriod period,
+            Func<Domain.DataMatch, object> value)
+        {
+            Heading = heading;
+            this.period = period;
+            valueExtractor = value;
+            IsLocked = this.period.DataLocks.Contains(data);
+            ActiveDataLock = IsLocked ? data.ToString() : "-";
+        }
+
+        public DataLockRowModel(
+            string heading,
+            Domain.CollectionPeriod period,
+            Func<Domain.DataMatch, object> value)
+            : this(heading, 0, period, value)
+        {
+        }
+    }
+}
