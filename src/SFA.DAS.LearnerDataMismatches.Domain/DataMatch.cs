9c26afc6a019ba8ac83dace563e77302b145ae96
dev4valtech <dev4valtech@gmail.com>
2020-08-27 16:56:38 +0100

    Refactored learner report

---
+++ src/SFA.DAS.LearnerDataMismatches.Domain/DataMatch.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace SFA.DAS.LearnerDataMismatches.Domain
+{
+    public class DataMatch
+    {
+        public long Ukprn { get; set; }
+        public long Uln { get; set; }
+        public short Standard { get; set; }
+        public short Framework { get; set; }
+        public short Program { get; set; }
+        public short Pathway { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime? PriceStart { get; set; }
+        public short AimSequenceNumber { get; set; }
+        public DateTime? PausedOn { get; set; }
+        public DateTime? StoppedOn { get; set; }
+        public DateTime? PlannedCompletion { get; set; }
+        public ApprenticeshipStatus CompletionStatus { get; set; }
+        public decimal Tnp1 { get; set; }
+        public decimal Tnp2 { get; set; }
+        public decimal Tnp3 { get; set; }
+        public decimal Tnp4 { get; set; }
+    }
+}
