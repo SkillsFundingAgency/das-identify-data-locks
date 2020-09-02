21ebcb950a879c4bd9f6b8b0f1a3ebb66957394d
dev4valtech <dev4valtech@gmail.com>
2020-09-02 14:42:49 +0100

    AD-2289 Added year range to AcademicYear model

--- src/SFA.DAS.LearnerDataMismatches.Domain/AcademicYear.cs
+++ src/SFA.DAS.LearnerDataMismatches.Domain/AcademicYear.cs
@@ -7,6 +7,9 @@ namespace SFA.DAS.LearnerDataMismatches.Domain
         public int Current { get; }
         public int Previous { get; }
 
+        public string CurrentRange { get; }
+        public string PreviousRange { get; }
+
         public AcademicYear(DateTime today)
         {
             var currentYear = int.Parse(today.ToString("yy"));
@@ -14,12 +17,16 @@ namespace SFA.DAS.LearnerDataMismatches.Domain
             if(today.Month >= 8)
             {
                 Current = getAcademicYear(currentYear, currentYear+1);
+                CurrentRange = $"{today.Year} - {today.Year+1}";
                 Previous = getAcademicYear(currentYear-1, currentYear);
+                PreviousRange = $"{today.Year-1} - {today.Year}";
             }
             else
             {
                 Current = getAcademicYear(currentYear-1, currentYear);
+                CurrentRange = $"{today.Year-1} - {today.Year}";
                 Previous = getAcademicYear(currentYear-2, currentYear-1);
+                PreviousRange = $"{today.Year-2} - {today.Year-1}";
             }
         }
 
