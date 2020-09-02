672bd265a41b4581ca57aadc7531385cb159f6e9
dev4valtech <dev4valtech@gmail.com>
2020-09-02 14:42:49 +0100

    AD-2289 Added year range to AcademicYear model

--- src/SFA.DAS.LearnerDataMismatches.UnitTests/Domain/AcademicYearTests.cs
+++ src/SFA.DAS.LearnerDataMismatches.UnitTests/Domain/AcademicYearTests.cs
@@ -7,19 +7,33 @@ namespace SFA.DAS.LearnerDataMismatches.UnitTests.Domain
 {
     public class AcademicYearTests
     {
-        static object[] TestData = 
+        static object[] AcademicYearTestData = 
         {
             new object[] {new DateTime(2019,8,1), 1920, 1819},
             new object[] {new DateTime(2019,7,31), 1819, 1718}
         };
 
-        [TestCaseSource("TestData")]
+        [TestCaseSource("AcademicYearTestData")]
         public void GenerateCorrectAcademicYears(DateTime today, int expectedCurrentYear, int expectedPreviousYear)
         {
             var model = new AcademicYear(today);
 
             model.Current.Should().Be(expectedCurrentYear);
             model.Previous.Should().Be(expectedPreviousYear);
+        }
+        static object[] AcademicYearRangeTestData = 
+        {
+            new object[] {new DateTime(2019,8,1), "2019 - 2020", "2018 - 2019"},
+            new object[] {new DateTime(2019,7,31), "2018 - 2019", "2017 - 2018"}
+        };
+
+        [TestCaseSource("AcademicYearRangeTestData")]
+        public void GenerateCorrectAcademicYearRanges(DateTime today, string expectedCurrentYearRange, string expectedPreviousYearRange)
+        {
+            var model = new AcademicYear(today);
+
+            model.CurrentRange.Should().Be(expectedCurrentYearRange);
+            model.PreviousRange.Should().Be(expectedPreviousYearRange);
         }
     }
 }
