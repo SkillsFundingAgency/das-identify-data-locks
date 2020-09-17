7c81903bd1cdef3bad8d209d953e5c56e1dbade9
dev4valtech <dev4valtech@gmail.com>
2020-09-17 09:16:25 +0100

    AD-2354 Removed base dependency in PauseDate model

--- src/SFA.DAS.IdentifyDataLocks.Web/Pages/_PauseDateDataLockRowModel.cshtml.cs
+++ src/SFA.DAS.IdentifyDataLocks.Web/Pages/_PauseDateDataLockRowModel.cshtml.cs
@@ -3,18 +3,25 @@ using SFA.DAS.IdentifyDataLocks.Domain;
 
 namespace SFA.DAS.IdentifyDataLocks.Web.Pages
 {
-    public class PauseDateDataLockRowModel : DataLockRowModel
+    public class PauseDateDataLockRowModel
     {
         private readonly Func<DataMatch, (DateTime? pausedOn, DateTime? resumedOn)> dataFunc;
         public string PausedOnDate { get; }
         public string ResumedOnDate { get; }
         public bool HasPausedDate { get; }
+        public string Heading { get; }
+        public string ActiveDataLock { get; }
+        public bool IsLocked { get; }
+
         public PauseDateDataLockRowModel(
             CollectionPeriod period,
             DataLock dataLock,
             string heading,
-            Func<DataMatch, (DateTime? pausedOn, DateTime? resumedOn)> dataFunc) : base(period, dataLock, heading)
+            Func<DataMatch, (DateTime? pausedOn, DateTime? resumedOn)> dataFunc) 
         {
+            Heading = heading;
+            IsLocked = period.DataLocks.Contains(dataLock);
+            ActiveDataLock = IsLocked ? dataLock.ToString() : "-";
             (DateTime? pausedOnDate, DateTime? resumedOnDate) = dataFunc.Invoke(period.Apprenticeship);
             HasPausedDate = pausedOnDate.HasValue;
             PausedOnDate = pausedOnDate?.ToShortDateString();
