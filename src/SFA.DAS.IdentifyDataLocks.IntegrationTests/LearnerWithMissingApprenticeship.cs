24c9ea6a3247e5ef3df908cb9c8e15fbe41f014b
dev4valtech <dev4valtech@gmail.com>
2020-09-24 11:26:47 +0100

    AD-2381 Added tests for missing apprenticeship

---
+++ src/SFA.DAS.IdentifyDataLocks.IntegrationTests/LearnerWithMissingApprenticeship.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NSubstitute;
+using NUnit.Framework;
+using SFA.DAS.IdentifyDataLocks.IntegrationTests.Helpers;
+using SFA.DAS.IdentifyDataLocks.Web.Pages;
+using SFA.DAS.Payments.Model.Core.Audit;
+
+namespace SFA.DAS.IdentifyDataLocks.IntegrationTests
+{
+    public class LearnerWithMissingApprenticeship
+    {
+        [SetUp]
+        public async Task Arrange()
+        {
+            await Testing.Reset();
+
+            await Testing.Context.AddEntitiesFromJsonResource<EarningEventModel>("SFA.DAS.IdentifyDataLocks.IntegrationTests.TestData.LearnerWithMissingApprenticeship.EarningEvents.json");
+
+            var dlocks = JsonConvert.DeserializeObject<DataLockEventModel[]>(
+                Resources.LoadAsString("SFA.DAS.IdentifyDataLocks.IntegrationTests.TestData.LearnerWithMissingApprenticeship.Dlock02_DataLocks.json"));
+
+            await Testing.Context.AddEntities(dlocks);
+
+            Testing.TimeProvider.Today.Returns(new DateTime(2019, 8, 1));
+        }
+
+        [Test]
+        public async Task History_only_contains_active_provider()
+        {
+            var learner = Testing.CreatePage<LearnerModel>();
+            learner.Uln = "3123456789";
+            await learner.OnGetAsync();
+
+            learner.CurrentYearDataLocks.Should().NotBeEmpty();
+        }
+    }
+}
