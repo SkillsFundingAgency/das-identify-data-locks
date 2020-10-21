25c3aa866b1a71668a639866aa99ab75271493d8
Ben Arnold <ben@arnoldfamily.me.uk>
2020-10-21 11:07:07 +0100

    🐛Test that TNPs are rendered with £ prefix

--- src/SFA.DAS.IdentifyDataLocks.IntegrationTests/LearnerWithStoppedApprenticeship/LearnerWithStoppedApprenticeship.cs
+++ src/SFA.DAS.IdentifyDataLocks.IntegrationTests/LearnerWithStoppedApprenticeship/LearnerWithStoppedApprenticeship.cs
@@ -18,4 +18,21 @@ namespace SFA.DAS.IdentifyDataLocks.IntegrationTests.LearnerWithStoppedApprentic
             result.Content.Headers.ContentType.ToString().Should().Be("text/html; charset=utf-8");
         }
     }
+
+    [Explicit]
+    public class LearnerWithMultiplePriceEpisodes : WebApplicationTestFixture
+    {
+        [Test]
+        public async Task RendersTNPs()
+        {
+            await Arrange("SFA.DAS.IdentifyDataLocks.IntegrationTests.TestData");
+
+            var result = await CreateClient().GetAsync("/Learner/2839925663");
+
+            result.EnsureSuccessStatusCode(); // Status Code 200-299
+            result.Content.Should().NotBeNull();
+            var body = await result.Content.ReadAsStringAsync();
+            body.Should().Contain("&#xA3;24,872.00");
+        }
+    }
 }
