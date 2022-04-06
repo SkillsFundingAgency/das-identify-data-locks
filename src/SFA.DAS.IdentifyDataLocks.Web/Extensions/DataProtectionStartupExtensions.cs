c95aeaa054d62db6f44583a1b651d766b1eeacb5
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-04-06 09:40:00 +0100

    [CON-4178] Add redis data protection on startup

---
+++ src/SFA.DAS.IdentifyDataLocks.Web/Extensions/DataProtectionStartupExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SFA.DAS.IdentifyDataLocks.Web.Infrastructure;
+using StackExchange.Redis;
+
+namespace SFA.DAS.IdentifyDataLocks.Web.Extensions
+{
+    public static class DataProtectionStartupExtensions
+    {
+        public static IServiceCollection AddDataProtection(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            if (!environment.IsDevelopment())
+            {
+                var config = configuration.GetSection(nameof(RedisConnectionSettings)).Get<RedisConnectionSettings>();
+
+                if (config != null)
+                {
+                    var redisConnectionString = config.RedisConnectionString;
+                    var dataProtectionKeysDatabase = config.DataProtectionKeysDatabase;
+
+                    var redis = ConnectionMultiplexer
+                        .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
+
+                    services.AddDataProtection()
+                        .SetApplicationName("das-identify-data-locks-web")
+                        .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+                }
+            }
+
+            return services;
+        }
+    }
+}
