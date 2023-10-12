ec4c1910e24981dbe351610df98ad8c01a2f730a
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-10-12 14:52:29 +0100

    Commit Details:
    1. Amended Access Denied Page for DfESignIn Users.
    2. Unit Testing included with the test coverage for newly added code.
    3. DfESignIn Auth PKG upgraded to the Latest v17.1.56
    
    Story: https://skillsfundingagency.atlassian.net/browse/FAI-939

---
+++ src/SFA.DAS.IdentifyDataLocks.UnitTests/Web/Error/WhenAccessDeniedOnGet.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.IdentifyDataLocks.Web.Constants;
+using SFA.DAS.IdentifyDataLocks.Web.Pages;
+
+namespace SFA.DAS.IdentifyDataLocks.UnitTests.Web.Error
+{
+    public class WhenAccessDeniedOnGet
+    {
+        [TestCase("test", "https://test-services.signin.education.gov.uk/approvals/select-organisation?action=request-service", true)]
+        [TestCase("pp", "https://test-services.signin.education.gov.uk/approvals/select-organisation?action=request-service", true)]
+        [TestCase("local", "https://test-services.signin.education.gov.uk/approvals/select-organisation?action=request-service", false)]
+        [TestCase("prd", "https://services.signin.education.gov.uk/approvals/select-organisation?action=request-service", false)]
+        [TestCase("", "https://services.signin.education.gov.uk/approvals/select-organisation?action=request-service", false)]
+        public void Then_Return_SamePropertyValue(string env, string helpLink, bool useDfESignIn)
+        {
+            //arrange
+            var mockConfiguration = new Mock<IConfiguration>();
+            var mockDfESignInSection = new Mock<IConfigurationSection>();
+            var mockResourceEnvSection = new Mock<IConfigurationSection>();
+
+            mockDfESignInSection.Setup(a => a.Value).Returns(useDfESignIn.ToString());
+            mockResourceEnvSection.Setup(a => a.Value).Returns(env);
+
+            mockConfiguration.Setup(a => a.GetSection(ConfigKey.ResourceEnvironmentName)).Returns(mockResourceEnvSection.Object);
+            mockConfiguration.Setup(a => a.GetSection(ConfigKey.UseDfESignIn)).Returns(mockDfESignInSection.Object);
+
+            //sut
+            var model = new AccessDenied(mockConfiguration.Object);
+            model.OnGet();
+
+            //assert
+            model.UseDfESignIn.Should().Be(useDfESignIn);
+            model.HelpPageLink.Should().Be(helpLink);
+        }
+    }
+}
---
+++ src/SFA.DAS.IdentifyDataLocks.Web/Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.IdentifyDataLocks.Web.Constants;
+using System;
+
+namespace SFA.DAS.IdentifyDataLocks.Web.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class AccessDenied : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        private string _integrationUrlPart = string.Empty;
+        private string _environment = string.Empty;
+
+        public bool UseDfESignIn { get; set; }
+
+        public AccessDenied(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            UseDfESignIn = _configuration.GetValue<bool>(ConfigKey.UseDfESignIn);
+            _environment = _configuration.GetValue<string>(ConfigKey.ResourceEnvironmentName);
+        }
+        
+
+        /// <summary>
+        /// Gets DfESignIn Select service link.
+        /// </summary>
+        public string HelpPageLink
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(_environment) && !_environment.Equals("prd", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    _integrationUrlPart = "test-";
+                }
+                return $"https://{_integrationUrlPart}services.signin.education.gov.uk/approvals/select-organisation?action=request-service";
+            }
+        }
+    }
+}
